7d9226afe47a2185faa79183e7d1067be3cafbe9
lissaanantha <lissaanantha1201@gmail.com>
2026-08-17 09:07:39 +0700

    update

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,8 +1,8 @@
 // assets/js/config.js
 
 // Ganti URL dan Key di bawah ini dengan project Supabase Anda
-const SUPABASE_URL = 'https://YOUR-SUPABASE-URL.supabase.co';
-const SUPABASE_KEY = 'YOUR-SUPABASE-ANON-KEY';
+const SUPABASE_URL = 'https://ztgjmyjrtrlyrgntvtny.supabase.co';
+const SUPABASE_KEY = 'sb_publishable_GGh8txjx_3OFYwmTGdccjQ_zkxRYqep';
 
 // Inisialisasi Supabase Client
 const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
